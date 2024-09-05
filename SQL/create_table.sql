f1a0c8ecd7cc189a9e68e0dd05e5e48665810989
ColaBlack <2698360477@qq.com>
2024-09-05 19:39:29 +0800

    初始提交

---
+++ SQL/create_table.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS user_center;
+
+USE user_center;
---
+++ src/main/java/edu/zafu/user_center_backend/UserCenterBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.zafu.user_center_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterBackendApplication.class, args);
+    }
+
+}
