11abe180c5e93cfb1f4e61741890cc10edc109d2
ColaBlack <2698360477@qq.com>
2024-09-05 21:38:51 +0800

    update 设置了接口文档的版权信息

---
+++ src/main/java/edu/zafu/user_center_backend/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package edu.zafu.user_center_backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ColaBlack
+ */
+@Configuration
+public class SwaggerConfig {
+ 
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("接口文档")
+                        //描叙
+                        .description("用户中心后端API接口文档")
+                        //版本
+                        .version("v1")
+                        //作者信息，自行设置
+                        .contact(new Contact().name("ColaBlack"))
+                        //设置接口文档的许可证信息
+                        .license(new License().name("Apache 2.0")));
+    }
+ 
+}
