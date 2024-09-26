96eef39c192a9a99e008d342796d2c01b8ac8b7c
ColaBlack <2698360477@qq.com>
2024-09-26 19:29:43 +0800

    perf(user表): 补充添加两个忘记添加到git的文件
    user.java
    userMapper.java

---
+++ src/main/java/edu/zafu/user_center_backend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package edu.zafu.user_center_backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.zafu.user_center_backend.model.PO.User;
+
+/**
+ * @author ColaBlack
+ * @description 针对表【user】的数据库操作Mapper
+ * @createDate 2024-09-26 19:04:07
+ * @Entity edu.zafu.user_center_backend.model.PO.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/edu/zafu/user_center_backend/model/PO/User.java
@@ -0,0 +1,62 @@
+package edu.zafu.user_center_backend.model.PO;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author ColaBlack
+ * @TableName user
+ */
+@TableName(value = "user")
+@Data
+public class User implements Serializable {
+    @Serial
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+    /**
+     * 用户id
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long userId;
+    /**
+     * 用户昵称
+     */
+    private String userNickname;
+    /**
+     * 用户账号
+     */
+    private String userAccount;
+    /**
+     * 用户头像
+     */
+    private String userAvatar;
+    /**
+     * 密码
+     */
+    private String userPassword;
+    /**
+     * 状态 0-正常 1-vip 2-ban 3-管理员
+     */
+    private Integer userRole;
+    /**
+     * 记录创建时间
+     */
+    private Date createTime;
+    /**
+     * 记录更新时间
+     */
+    private Date updateTime;
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+    /**
+     * 用户简介
+     */
+    private String userProfile;
+}
