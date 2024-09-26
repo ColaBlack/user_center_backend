d66491ad14829c506d86bafb727b5a902ecc1764
ColaBlack <2698360477@qq.com>
2024-09-26 19:03:06 +0800

    perf(数据库user表): 优化了表结构

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -3,20 +3,19 @@ CREATE DATABASE IF NOT EXISTS user_center;
 USE user_center;
 
 # 用户表
-
 CREATE TABLE IF NOT EXISTS user
 (
-    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username     VARCHAR(256)                       NULL COMMENT '用户昵称',
-    userAccount  VARCHAR(256)                       NULL COMMENT '用户账号',
-    avatar       VARCHAR(1024)                      NULL COMMENT '用户头像',
-    gender       TINYINT                            NULL COMMENT '性别',
-    userPassword VARCHAR(512)                       NOT NULL COMMENT '密码',
-    email        VARCHAR(512)                       NULL COMMENT '邮箱',
-    userStatus   INT      DEFAULT 0                 NULL COMMENT '状态 0-正常 1-vip 2-ban 3-管理员',
-    phone        VARCHAR(128)                       NULL COMMENT '电话',
-    createTime   DATETIME DEFAULT CURRENT_TIMESTAMP NULL COMMENT '记录创建时间',
-    updateTime   DATETIME DEFAULT CURRENT_TIMESTAMP null ON UPDATE CURRENT_TIMESTAMP COMMENT '记录更新时间',
-    isDelete    TINYINT DEFAULT 0 NOT NULL COMMENT '是否删除',
-    userProfile TEXT              NULL COMMENT '用户简介'
+    user_id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_nickname VARCHAR(256) DEFAULT '快来设置昵称吧'  NULL COMMENT '用户昵称',
+    user_account  VARCHAR(256)                           NULL COMMENT '用户账号',
+    user_avatar   VARCHAR(1024)                          NULL COMMENT '用户头像',
+    user_password VARCHAR(512)                           NOT NULL COMMENT '密码',
+    user_role     INT          DEFAULT 0                 NULL COMMENT '状态 0-正常 1-vip 2-ban 3-管理员',
+    create_time   DATETIME     DEFAULT CURRENT_TIMESTAMP NULL COMMENT '记录创建时间',
+    update_time   DATETIME     DEFAULT CURRENT_TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '记录更新时间',
+    is_delete     TINYINT      DEFAULT 0                 NOT NULL COMMENT '是否删除',
+    user_profile  TEXT                                   NULL COMMENT '用户简介'
 );
+
+CREATE INDEX user_user_account_index
+    ON user (user_account);
